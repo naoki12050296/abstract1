1c0a02cdccb468392d7881d848e34da405aae96a
naoki12050296 <n_h.noo_bow.125@icloud.com>
2017-09-30 21:05:56 +0900

    code commit

--- AndroidStudioProjects/Calculator/app/src/main/java/com/example/naokihonda/calculator/Form.java
+++ AndroidStudioProjects/Calculator/app/src/main/java/com/example/naokihonda/calculator/Form.java
@@ -1,15 +1,19 @@
 package com.example.naokihonda.calculator;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.lang.reflect.InvocationTargetException;
 
@@ -17,11 +21,8 @@ import java.lang.reflect.InvocationTargetException;
  * Created by naokihonda on 2017/09/18.
  */
 
-public class Form extends AppCompatActivity{
 
-    public static String EXTRA_SUMPRICE = "com.example.naokihonda.calculator_SUMPRICE";
-    public static String EXTRA_PERSONS = "com.example.naokihonda.calculator_PERSONS";
-    public static Integer warikan = 0;
+public class Form extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +30,19 @@ public class Form extends AppCompatActivity{
         setContentView(R.layout.activity_main);
     }
 
+    public static String EXTRA_SUMPRICE = "com.example.naokihonda.calculator_SUMPRICE";
+    public static String EXTRA_PERSONS = "com.example.naokihonda.calculator_PERSONS";
+    public static Integer warikan = 0;
+
+
     //電卓に計算させる
-    public void calculation(View view)throws InvocationTargetException {
+    public void calculation(View view) {
 
         //EditTextを取得
         EditText sumprice = (EditText) findViewById(R.id.sumPrice);
         EditText persons = (EditText) findViewById(R.id.persons);
         EditText campaign = (EditText) findViewById(R.id.campaign);
+
         // ラジオグループのオブジェクトを取得
         RadioGroup rg = (RadioGroup) findViewById(R.id.radiogroup);
 
@@ -44,66 +51,81 @@ public class Form extends AppCompatActivity{
         String Persons = persons.getText().toString().trim();
         String Campaign = campaign.getText().toString().trim();
 
-        //int型に変換する
-        int sum_price = Integer.parseInt(sumPrice);
-        int sum_persons = Integer.parseInt(Persons);
-
-
         //中身をみて条件分岐
         if (sumPrice.equals("")) {
             sumprice.setError("合計金額が入っていません");
         } else if (Persons.equals("")) {
             persons.setError("合計人数が入っていません");
-        } else if (sum_price <= sum_persons) {
-            persons.setError("合計人数が合計金額を超えています");
-        } else if (Campaign.equals("")) {
-            warikan = sum_price / sum_persons;
         } else {
-            campaign = (EditText) findViewById(R.id.campaign);
-            Campaign = campaign.getText().toString().trim();
-            int CamPaign = Integer.parseInt(Campaign);
-            warikan = (sum_price - CamPaign) / sum_persons;
+            //int型に変換する
+            int sum_price = Integer.parseInt(sumPrice);
+            int sum_persons = Integer.parseInt(Persons);
+
+            if (sum_price <= sum_persons) {
+                persons.setError("合計人数が合計金額を超えています");
+            } else if (Campaign.equals("")) {
+                warikan = sum_price / sum_persons;
+            } else {
+                campaign = (EditText) findViewById(R.id.campaign);
+                Campaign = campaign.getText().toString().trim();
+                int CamPaign = Integer.parseInt(Campaign);
+                warikan = (sum_price - CamPaign) / sum_persons;
+            }
+
+            // チェックされているラジオボタンの ID を取得
+            int CheckedId = rg.getCheckedRadioButtonId();
+
+            // チェックされているラジオボタンオブジェクトを取得
+            RadioButton radioButton = (RadioButton) findViewById(CheckedId);
+
+            // getId()でラジオボタンを識別し、ラジオボタンごとの処理を行う
+            boolean checked = radioButton.isChecked();
+            switch (radioButton.getId()) {
+                case R.id.rounddown_100:
+                    if (checked) {
+                        warikan = (warikan / 100) + 1;
+                        warikan *= 100;
+                    }
+                    break;
+                case R.id.rounddown_500:
+                    if (checked) {
+                        warikan = (warikan / 500) + 1;
+                        warikan *= 500;
+                    }
+                    break;
+                case R.id.rounddown_1000:
+                    if (checked) {
+                        warikan = (warikan / 1000) + 1;
+                        warikan *= 1000;
+                    }
+                    break;
+                case R.id.radioButton_not:
+                    if (checked) {
+                    }
+                    break;
+                default:
+                    break;
+            }
+            //次の画面へ
+            Intent intent = new Intent(getApplication(), result.class);
+            intent.putExtra("EXTRA_WARIKAN", warikan);
+            startActivityForResult(intent, warikan);
         }
+    }
 
-        // チェックされているラジオボタンの ID を取得
-        int CheckedId = rg.getCheckedRadioButtonId();
+    public void listView(View view) {
 
-        // チェックされているラジオボタンオブジェクトを取得
-        RadioButton radioButton = (RadioButton) findViewById(CheckedId);
+       /* // listボタンのClickリスナー
+        View.OnClickListener list_ClickListener = new View.OnClickListener(){
+            public void onClick(View v) {
+                list_Click(v);
+            }
+        };
 
-        // getId()でラジオボタンを識別し、ラジオボタンごとの処理を行う
-        boolean checked = radioButton.isChecked();
-        switch (radioButton.getId()) {
-            case R.id.rounddown_100:
-                if (checked) {
-                    warikan /= 100;
-                    warikan *= 100;
-                }
-                break;
-            case R.id.rounddown_500:
-                if (checked) {
-                    warikan /= 500;
-                    warikan *= 500;
-                }
-                break;
-            case R.id.rounddown_1000:
-                if (checked) {
-                    warikan /= 1000;
-                    warikan *= 1000;
-                }
-                break;
-            case R.id.radioButton_not:
-                if(checked){
-                }
-                break;
-            default:
-                break;
-        }
+        Button buttonDelete = (Button)this.findViewById(R.id.list);
+        buttonDelete.setOnClickListener(list_ClickListener);*/
 
-        //次の画面へ
-        Intent intent = new Intent(getApplication(), result.class);
-        intent.putExtra("EXTRA_WARIKAN", warikan);
-        startActivityForResult(intent, warikan);
-
+        Intent intent = new Intent(getApplication(), List.class);
+        startActivity(intent, null);
     }
 }
--- AndroidStudioProjects/Calculator/app/src/main/java/com/example/naokihonda/calculator/result.java
+++ AndroidStudioProjects/Calculator/app/src/main/java/com/example/naokihonda/calculator/result.java
@@ -1,13 +1,42 @@
 package com.example.naokihonda.calculator;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
 
 import static com.example.naokihonda.calculator.Form.warikan;
+import static com.example.naokihonda.calculator.R.string.persons;
+import static com.example.naokihonda.calculator.R.string.sum_price;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+import io.realm.RealmList;
+import io.realm.RealmObject;
+import io.realm.annotations.Ignore;
+import io.realm.annotations.PrimaryKey;
 
 public class result extends AppCompatActivity {
+
+    // InsertボタンのClickリスナー
+    private View.OnClickListener buttonInsert_ClickListener = new View.OnClickListener(){
+        public void onClick(View v) {buttonInsert_Click(v);}};
+    /* UpdateボタンのClickリスナー */
+    private View.OnClickListener buttonUpdate_ClickListener = new View.OnClickListener(){
+        public void onClick(View v) {buttonUpdate_Click(v);}};
+    /* DeleteボタンのClickリスナー */
+    private View.OnClickListener buttonDelete_ClickListener = new View.OnClickListener(){
+        public void onClick(View v) {buttonDelete_Click(v);}};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +44,118 @@ public class result extends AppCompatActivity {
         setContentView(R.layout.activity_result);
 
         Intent intent = getIntent();
-        int i = intent.getIntExtra("EXTRA_WARIKAN",warikan);
-        TextView resultLabel = (TextView)findViewById(R.id.resultLabel);
-        resultLabel.setText(String.valueOf(i));
+        int w = intent.getIntExtra("EXTRA_WARIKAN", warikan);
+        TextView resultLabel = (TextView) findViewById(R.id.resultLabel);
+        resultLabel.setText(String.valueOf(w) + "円");
 
+        //ボタンにClickリスナーを設定する。
+        Button buttonInsert = (Button)this.findViewById(R.id.buttonInsert);
+        buttonInsert.setOnClickListener(buttonInsert_ClickListener);
+        Button buttonUpdate = (Button)this.findViewById(R.id.buttonUpdate);
+        buttonUpdate.setOnClickListener(buttonUpdate_ClickListener);
+        Button buttonDelete = (Button)this.findViewById(R.id.buttonDelete);
+        buttonDelete.setOnClickListener(buttonDelete_ClickListener);
     }
 
+    /*
+     * InsertボタンClick処理
+     */
+    private void buttonInsert_Click(View v){
+        ContentValues values = new ContentValues();
+        values.put("Result", warikan);
+
+        UserOpenHelper dbHelper = new UserOpenHelper(this);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        long ret;
+        try {
+            ret = db.insert("MyTable", null, values);
+        } finally {
+            db.close();
+        }
+        if (ret == -1) {
+            Toast.makeText(this, "Insert失敗", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Insert成功", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /*
+      UpdateボタンClick処理*/
+
+    private void buttonUpdate_Click(View v){
+        ContentValues values = new ContentValues();
+        values.put("Age",24);
+        String whereClause = "No = ?";
+        String whereArgs[] = new String[1];
+        whereArgs[0] = "1";
+
+        UserOpenHelper dbHelper = new UserOpenHelper(this);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        int ret;
+        try {
+            ret = db.update("MyTable", values, whereClause, whereArgs);
+        } finally {
+            db.close();
+        }
+        if (ret == -1){
+            Toast.makeText(this, "Update失敗", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Update成功", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /* DeleteボタンClick処理 */
+    private void buttonDelete_Click(View v){
+
+        deleteDatabase("cal.db");
+        Toast.makeText(this, "Delete成功", Toast.LENGTH_SHORT).show();
+
+        /*String whereClause = "No = ?";
+        String whereArgs[] = new String[1];
+        whereArgs[0] = "1";
+
+        UserOpenHelper dbHelper = new UserOpenHelper(this);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        int ret;
+        try {
+            ret = db.delete("MyTable", whereClause, whereArgs);
+        } finally {
+            db.close();
+        }
+        if (ret == -1){
+            Toast.makeText(this, "Delete失敗", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Delete成功", Toast.LENGTH_SHORT).show();
+        }*/
+    }
 }
+
+
+
+    /*
+        //Realmインスタンス取得
+        Realm realm = Realm.getInstance(this);
+
+        //トランザクション開始
+        realm.beginTransaction();
+
+        //Realmインスタンス化
+        User user = new User();
+
+        //値をセット
+        user.setSum_price(sum_price);
+        user.setPersons(persons);
+        user.setResult(warikan);
+
+        realm.commitTransaction();
+
+        realm.close();
+        */
+
+
+
+
+
+
+
+
